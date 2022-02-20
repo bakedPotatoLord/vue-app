4b8a046a528c4070fb84187d9c7f788105c288bc
josiah <918780@dpsk12.net>
2022-02-20 08:32:58 -0700

    continued playing with vue

--- src/App.vue
+++ src/App.vue
@@ -15,34 +15,18 @@ export default {
     },
     notify() {
       alert('navigation was prevented.')
+    },
+    pushMessage(){
+      alert('navigation was prevented.')
     }
   }
 }
 </script>
 
 <template>
-  <!--
-    Note we don't need .value inside templates because
-    refs are automatically "unwrapped" in templates.
-  -->
-  <h1>{{ message }}</h1>
-
-  <!--
-    Bind to a method/function.
-    The @click syntax is short for v-on:click.
-  -->
-  <button @click="reverseMessage">Reverse Message</button>
-
-  <!-- Can also be an inline expression statement -->
-  <button @click="message += '!'">Append "!"</button>
-
-  <!--
-    Vue also provides modifiers for common tasks
-    such as e.preventDefault() and e.stopPropagation()
-  -->
-  <a href="https://vuejs.org" @click.prevent="notify">
-    A link with e.preventDefault()
-  </a>
+  <input v-model="message" placeholder="edit me"><br>
+  <button @click="pushMessage()"></button>
+  <p>Message is: {{ message }}</p>
 </template>
 
 <style>
